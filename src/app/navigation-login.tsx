66f3f2ea6fab409eed864da31b52dfccc9ad6a28
jyotishn <jyotish@netcomlearning.com>
2024-01-24 00:33:44 +0530

    Build issue fixed

--- src/app/navigation-login.tsx
+++ src/app/navigation-login.tsx
@@ -13,14 +13,14 @@ const NavigationLogin = () => {
     <nav className="global-header navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
         <div className='nav-logo'>
-          <a className="navbar-brand" href="/">
+          <Link className="navbar-brand" href="/">
             <Image
               src='/logo.svg'
               layout='fill'
               objectFit="contain"
               alt='AI Certs logo'
             />
-          </a>
+          </Link>
         </div>
         {/* <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
