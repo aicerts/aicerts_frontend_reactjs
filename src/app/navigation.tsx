7a2a293b1a65d92ab378cf6074df8faeff42795e
jyotishn <jyotish@netcomlearning.com>
2024-02-29 22:06:57 +0530

    Profile feature

--- src/app/navigation.tsx
+++ src/app/navigation.tsx
@@ -1,13 +1,17 @@
 import { logout } from '@/common/auth';
-import Image from 'next/image';
+import Image from 'next/legacy/image';
 import Link from 'next/link';
 import React, { useEffect, useRef  } from 'react';
-import {Navbar, Button} from 'react-bootstrap';
+import {Navbar, Container, NavDropdown, ButtonGroup } from 'react-bootstrap';
 import { useRouter } from 'next/router';
 
 const Navigation = () => {
   const router = useRouter();
   const isUserLoggedIn = useRef(false); // Use useRef instead of a variable
+
+  const handleViewProfile = () => {
+    window.location.href = "/"
+  } 
   
   useEffect(() => {
     isUserLoggedIn.current = localStorage?.getItem('user') !== null; // Update the ref value
@@ -22,51 +26,81 @@ const Navigation = () => {
   const routesWithLogoutButton = ['/certificates', '/issue-pdf-certificate', '/issue-certificate'];
   return (
     <>
-      <nav className="global-header navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid">
-          <div className='d-flex justify-content-between align-items-center w-100'>
-          <div className='nav-logo'>
-            <Link className="navbar-brand" href="/certificates">
-              <Image
-                src='https://images.netcomlearning.com/ai-certs/Certs365-logo.svg'
-                layout='fill'
-                objectFit="contain"
-                alt='AI Certs logo'
-              />
-            </Link>
-          </div>
-          {routesWithLogoutButton.includes(router.pathname) && (
-              <div className='nav-logo logout'>
-                <button className="btn btn-link" onClick={handleLogout}>
+      <Navbar className="global-header navbar navbar-expand-lg navbar-light bg-light">
+        <Container fluid>
+          <Navbar.Brand href="/certificates">
+            <div className='nav-logo'>
+              <Link className="navbar-brand" href="/certificates">
+                <Image
+                  src='https://images.netcomlearning.com/ai-certs/Certs365-logo.svg'
+                  layout='fill'
+                  objectFit="contain"
+                  alt='AI Certs logo'
+                />
+              </Link>
+            </div>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+          <Navbar.Collapse className="justify-content-end">
+            <Navbar.Text>
+              <NavDropdown 
+                as={ButtonGroup}
+                align={{ md: 'end' }}
+                title={
+                  <div className='picture'>
+                    <span>jd</span>
+                    <div className='dropdown-arrow'>
+                      <Image 
+                        src="https://images.netcomlearning.com/ai-certs/icons/down-arrow.svg"
+                        layout='fill'
+                        objectFit='contain'
+                        alt='Dropdown Menu'
+                      />
+                    </div>
+                  </div>
+                  
+                }
+                className='profile'
+               >
+                <div className='user-info'>
+                  <div className='divider'>
+                    <div className='info'>
+                        <span className='label'>Attribute #01</span>
+                        <span className='data'>Value</span>
+                    </div>
+                    <div className='info'>
+                        <span className='label'>Attribute #01</span>
+                        <span className='data'>Value</span>
+                    </div>
+                  </div>
+                  <div className='divider'>
+                    <div className='info'>
+                        <span className='label'>Attribute #01</span>
+                        <span className='data'>Value</span>
+                    </div>
+                    <div className='info'>
+                        <span className='label'>Attribute #01</span>
+                        <span className='data'>Value</span>
+                    </div>
+                  </div>
+                </div>
+              </NavDropdown>
+            </Navbar.Text>
+            <Navbar.Text>
+              {routesWithLogoutButton.includes(router.pathname) && (
+                <div className='logout' onClick={handleLogout}>
                   <Image
                     src='https://images.netcomlearning.com/ai-certs/logout.svg'
                     layout='fill'
                     objectFit="contain"
                     alt='logout Icon'
                   />
-                </button>
-              </div>
-            )}
-          </div>
-          {/* <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button> */}
-
-         {/* <div style={{flexDirection:"row", justifyContent:"flex-end"}} className="collapse navbar-collapse" id="navbarSupportedContent"> 
-            <ul className="navbar-nav mr-auto">
-              <li className="nav-item">
-                <a className="nav-link" href="/varify-documents">Verify Document</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/varify-documents">Submit Document</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/profile-details">User Profile</a>
-              </li>
-            </ul> 
-          </div>  */}
-        </div>
-      </nav>
+                </div>
+              )}
+            </Navbar.Text>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </>
   );
 };
