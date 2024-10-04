b9bf13c6594586d6929ab0a15033085753791874
Basit Channa <basit@aicerts@io>
2024-10-04 19:25:59 +0530

    Changes

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -1,44 +1,94 @@
-import React from 'react'
-import { Col, Row } from 'react-bootstrap'
+import React, { useState } from 'react';
+import { Col, Form, Row } from 'react-bootstrap';
+import Button from '../../shared/button/button';
 
-const Settings = () => {
-  return (
-    <div className='page-bg'>
-            <div className='position-relative settings-container h-100'>
-                <div  className='settings-title' >
-            <h3>Settings</h3>
-                </div>
-
-       <div className='org-details'>
-                            <h2 className='title'>Organization Details</h2>
-                            <Row>
-                                    <Col xs={12} md={4}>
-                                        <div className='label'>Organization Name</div>
-                                        <div className='info'>-</div>
-                                    </Col>
-                                    <Col xs={12} md={4}>
-                                        <div className='label'>Organization Type</div>
-                                    </Col>
-                                    <Col xs={12} md={4}>
-                                        <div className='label'>Industry Sector</div>
-                                    </Col>
-                                    <Col xs={12} md={4}>
-                                        <div className='label'>Website</div>
-                                        <div className='info'>
-                                         
-                                        </div>
-                                    </Col>
-                                    <Col xs={12} md={4}>
-                                        <div className='label'>Address</div>
-                                        <div className='info'>
-                                        </div>
-                                    </Col>
-                            </Row>
-
-                        </div>
-    </div>
-    </div>
-  )
+interface DateRange {
+  from: string;
+  to: string;
 }
 
-export default Settings
+const Settings: React.FC = () => {
+  const [issuanceDate, setIssuanceDate] = useState<DateRange>({
+    from: '',
+    to: '',
+  });
+
+  // Adjust the event type to be more generic for React-Bootstrap Form.Control
+  const handleDateChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+    field: keyof DateRange
+  ) => {
+    setIssuanceDate({
+      ...issuanceDate,
+      [field]: e.target.value,
+    });
+  };
+
+  return (
+    <div className="page-bg">
+      <div className="position-relative settings-container h-100">
+        <div className="settings-title">
+          <h3>Settings</h3>
+        </div>
+
+        {/* Issuance Report */}
+        <div className="org-details mb-5">
+          <h2 className="title">Issuance Report</h2>
+          <Row className=" d-flex align-items-center justify-content-center mt-3">
+            <Col xs={12} md={4}>
+              <Form.Label  className='label-settings'>From:</Form.Label>
+              <Form.Control
+                type="date"
+                className="search-input-setting"
+                value={issuanceDate.from}
+                onChange={(e) => handleDateChange(e, 'from')}
+              />
+            </Col>
+            <Col xs={12} md={4}>
+              <Form.Label  className='label-settings'>To:</Form.Label>
+              <Form.Control
+                type="date"
+                className="search-input-setting"
+                value={issuanceDate.to}
+                onChange={(e) => handleDateChange(e, 'to')}
+              />
+            </Col>
+            <Col className='mt-4' xs={12} md={3}>
+              <Button label="Download" className="global-btn golden" />
+            </Col>
+          </Row>
+        </div>
+
+        {/* Invoice Report */}
+        <div className="org-details">
+          <h2 className="title">Invoice Report</h2>
+          <Row className=" d-flex align-items-center justify-content-center mt-3">
+            <Col xs={12} md={4}>
+              <Form.Label className='label-settings'>From:</Form.Label>
+              <Form.Control
+                type="date"
+                className="search-input-setting"
+                value={issuanceDate.from}
+                onChange={(e) => handleDateChange(e, 'from')}
+              />
+            </Col>
+            <Col xs={12} md={4}>
+              <Form.Label  className='label-settings'>To:</Form.Label>
+              <Form.Control
+                type="date"
+                className="search-input-setting"
+                value={issuanceDate.to}
+                onChange={(e) => handleDateChange(e, 'to')}
+              />
+            </Col>
+            <Col className='mt-4' xs={12} md={3}>
+              <Button label="Download" className="global-btn golden" />
+            </Col>
+          </Row>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Settings;
